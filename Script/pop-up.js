69c8627db5599e6b8582f7b5aaa67a32826af362
phone2644 <nicklove115533@gmail.com>
2025-02-19 01:12:15 +0700

    update-about pade

--- Script/pop-up.js
+++ Script/pop-up.js
@@ -16,3 +16,40 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    const popUps = document.querySelectorAll(".pop-up_down");
+
+    popUps.forEach(popUp => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    popUp.classList.add("show1");
+                   observer.unobserve(popUp);
+                }else {
+                    popUp.classList.remove("show1");
+                }
+            });
+        });
+
+        observer.observe(popUp);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUps = document.querySelectorAll(".img_card");
+
+    popUps.forEach(popUp => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    popUp.classList.add("show1");
+                   observer.unobserve(popUp);
+                }else {
+                    popUp.classList.remove("show1");
+                }
+            });
+        });
+
+        observer.observe(popUp);
+    });
+});
