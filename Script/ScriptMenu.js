2080b1e8573602ad0561775e7d0f29b361d08081
คุณากร ทำดี <kingmanstar55@gmail.com>
2025-02-25 17:33:21 +0700

    Update ScriptMenu.js

--- Script/ScriptMenu.js
+++ Script/ScriptMenu.js
@@ -3,7 +3,7 @@ function createNavbar() {
     
   <nav class="navbar navbar-expand-lg bg-body-tertiary"style="z-index: 999;">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"> <img src="Source/IMG_04511.png" width="130" height="94"></a>
+      <a class="navbar-brand" href=".index.html"> <img src="Source/IMG_04511.png" width="130" height="94"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
