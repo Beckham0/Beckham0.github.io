1ceb30b889a3aae375af1f253dd6eaa541d2ab48
phone2644 <nicklove115533@gmail.com>
2025-02-25 15:37:14 +0700

    translate index

--- Script/ScriptMenu.js
+++ Script/ScriptMenu.js
@@ -1,5 +1,6 @@
 function createNavbar() {
     const navbarHTML = `
+    
   <nav class="navbar navbar-expand-lg bg-body-tertiary"style="z-index: 999;">
     <div class="container-fluid">
       <a class="navbar-brand" href="#"> <img src="Source/IMG_04511.png" width="130" height="94"></a>
@@ -9,33 +10,33 @@ function createNavbar() {
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav " >
           <li class="nav-item">
-            <a class="nav-link a"  href="./Index.html">หน้าเเรก</a>
+            <a id="lang_Home" class="nav-link a"  href="./Index.html">หน้าเเรก</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link b" href="./Our_about.html">เกี่ยวกับเรา</a>
+            <a id="lang_About" class="nav-link b" href="./Our_about.html">เกี่ยวกับเรา</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle c" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a id="lang_Services" class="nav-link dropdown-toggle c" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               บริการเรา
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="รับทำบัญชี ปิดงบ ยืนภาษี.html">รับทำบัญชี ปิดงบ ยืนภาษี</a></li>
-              <li><a class="dropdown-item" href="บริการสอบบัญชี.html">บริการสอบบัญชี</a></li>
-              <li><a class="dropdown-item" href="รับวางระบบภาษี.html">รับวางระบบภาษี</a></li>
-              <li><a class="dropdown-item" href="บริการงานทะเบียน.html">บริการงานทะเบียน</a></li>
-              <li><a class="dropdown-item" href="บริการขอใบอนุญาติสถานที่เก็บอาหาร.html">บริการขอใบอนุญาติสถานที่เก็บอาหาร</a></li>
-              <li><a class="dropdown-item" href="บริการงานจดทะเบียนบริษัท.html">บริการงานจดทะเบียนบริษัท</a></li>
-              <li><a class="dropdown-item" href="บริการขอใบอนุญาตินำเข้า-ส่งออก.html">บริการขอใบอนุญาตินำเข้า-ส่งออก</a></li>
+              <li><a id="lang_item1" class="dropdown-item" href="รับทำบัญชี ปิดงบ ยืนภาษี.html">รับทำบัญชี ปิดงบ ยืนภาษี</a></li>
+              <li><a id="lang_item2" class="dropdown-item" href="บริการสอบบัญชี.html">บริการสอบบัญชี</a></li>
+              <li><a id="lang_item3" class="dropdown-item" href="รับวางระบบภาษี.html">รับวางระบบภาษี</a></li>
+              <li><a id="lang_item4" class="dropdown-item" href="บริการงานทะเบียน.html">บริการงานทะเบียน</a></li>
+              <li><a id="lang_item5" class="dropdown-item" href="บริการขอใบอนุญาติสถานที่เก็บอาหาร.html">บริการขอใบอนุญาติสถานที่เก็บอาหาร</a></li>
+              <li><a id="lang_item6" class="dropdown-item" href="บริการงานจดทะเบียนบริษัท.html">บริการงานจดทะเบียนบริษัท</a></li>
+              <li><a id="lang_item7" class="dropdown-item" href="บริการขอใบอนุญาตินำเข้า-ส่งออก.html">บริการขอใบอนุญาตินำเข้า-ส่งออก</a></li>
             </ul>
           </li>
           <li class="nav-item">
-            <a class="nav-link d" href="./our_work.html">ผลงานเรา</a>
+            <a id="lang_Work" class="nav-link d" href="./our_work.html">ผลงานเรา</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link e" href="./info.html" >ข่าวสาร</a>
+            <a  id="lang_News" class="nav-link e" href="./info.html" >ข่าวสาร</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link f" href="./contact_us.html" >ติดต่อเรา</a>
+            <a id="lang_Contant" class="nav-link f" href="./contact_us.html" >ติดต่อเรา</a>
           </li>
         </ul>
       </div>
@@ -58,33 +59,35 @@ function createfooter() {
     <div class="footer-container">
       <div class="footer-left">
         <div class="footer-left-1"><img src="Source/IMG_04511.png" alt="NRP Logo" class="logo"></div>
-        <div class="footer-left-1"><p class="company-name">บริษัท เอ็นอาร์พี มายแอคเคาท์ จำกัด</p>
-        <p class="address">
-          115/8 หมู่บ้านพฤกษา รังสิต-วงแหวน หมู่2 ตำบลลำผักกูด อำเภอธัญบุรี ปทุมธานี<br>
-          โทร 062-896-4535 อีเมล: info@nrpmyaccount.com
-        </p></div>
+        <div class="footer-left-1"><p  id="footer_1" class="company-name">บริษัท เอ็นอาร์พี มายแอคเคาท์ จำกัด</p>
+        <p id="footer_2" class="address">
+          115/8 หมู่บ้านพฤกษา รังสิต-วงแหวน หมู่2 ตำบลลำผักกูด อำเภอธัญบุรี ปทุมธานี</p>
+          <p id="footer_2_1" class="address">
+           โทร 062-896-4535 อีเมล: info@nrpmyaccount.com</p>
+         
+        </div>
       </div>
       <div class="footer-right">
         <div class="s-1">
         <div class="divider"></div>
         <div class="services">
           <ul>
-            <a href="#" class=""><li>สำนักงานบัญชีทรัปเพิ้ลเอิร์น</li></a>
-            <a href="#" class=""><li>เป็นที่ปรึกษาการด้านบัญชี</li></a>
-            <a href="#" class=""><li>รับทำบัญชีรายเดือน</li></a>
-            <a href="#" class=""><li>รับทำบัญชีรายปี</li></a>
-            <a href="#" class=""><li>ระบบงบการเงิน</li></a>
+            <a href="#" class=""><li id="footer_3">สำนักงานบัญชีทรัปเพิ้ลเอิร์น</li></a>
+            <a href="#" class=""><li id="footer_4">เป็นที่ปรึกษาการด้านบัญชี</li></a>
+            <a href="#" class=""><li id="footer_5">รับทำบัญชีรายเดือน</li></a>
+            <a href="#" class=""><li id="footer_6">รับทำบัญชีรายปี</li></a>
+            <a href="#" class=""><li id="footer_7">ระบบงบการเงิน</li></a>
           </ul>
         </div></div>
         <div class="s-1">
         <div class="divider"></div>
         <div class="services">
           <ul>
-            <a href="#" class=""><li>สำนักงานบัญชีทรัปเพิ้ลเอิร์น</li></a>
-            <a href="#" class=""><li>เป็นที่ปรึกษาการด้านบัญชี</li></a>
-            <a href="#" class=""><li>รับทำบัญชีรายเดือน</li></a>
-            <a href="#" class=""><li>รับทำบัญชีรายปี</li></a>
-            <a href="#" class=""><li>ระบบงบการเงิน</li></a>
+            <a href="#" class=""><li id="footer_8">สำนักงานบัญชีทรัปเพิ้ลเอิร์น</li></a>
+            <a href="#" class=""><li id="footer_9">เป็นที่ปรึกษาการด้านบัญชี</li></a>
+            <a href="#" class=""><li id="footer_10">รับทำบัญชีรายเดือน</li></a>
+            <a href="#" class=""><li id="footer_11">รับทำบัญชีรายปี</li></a>
+            <a href="#" class=""><li id="footer_12">ระบบงบการเงิน</li></a>
           </ul>
         </div></div>
       </div>
---
+++ Script/translate.js
@@ -0,0 +1,194 @@
+var th ={
+    lang_language:"Language: TH",
+//     lang_Home:"หน้าแรก",
+//     lang_About:"เกี่ยวกับเรา",
+//     lang_Services:"บริการเรา",
+//     lang_Work:"ผลงานเรา",
+//     lang_News:"ข่าวสาร ",
+//     lang_Contant:"ติดต่อ",
+    
+//     lang_item1:"รับทำบัญชี ปิดงบ ยืนภาษี",
+//     lang_item2:"บริการสอบบัญชี",
+//     lang_item3:"รับวางระบบภาษี ",
+//     lang_item4:"บริการงานทะเบียน ",
+//     lang_item5:"บริการขอใบอนุญาติสถานที่เก็บอาหาร ",
+//     lang_item6:"บริการงานจดทะเบียนบริษัท ",
+//     lang_item7:"บริการขอใบอนุญาตินำเข้า-ส่งออก ",
+//     lang_1:"บริษัท เอ็นอาร์พี มายแอคเคาท์ จำกัด",
+//     lang_2:"บริษัท เอ็นอาร์พี",
+//     lang_3:"มายแอคเคาท์ จำกัด  ",
+//     lang_4:"ทำความรู้จัก",
+//     lang_5:"บริษัท เอ็นอาร์พี มายแอคเคาท์ จำกัด",
+    // lang_6:" บริษัท เอ็นอาร์พี มายแอคเคาท์ จำกัด เป็นบริษัทบัญชีมืออาชีพ ที่มีประสบการณ์มากกว่า 10 ปีในการให้บริการบัญชีครบวงจรแก่ ธุรกิจทกขนาด ตั้งแต่ SME,Startupไปจนถึงบริษัทขามชาติใน หลากหลายอุตสาหกรรม ไม่ว่าจะเป็นอุตสาหกรรมการผลิต, การค้า, บริการ, หรือแม้แต่ธุรกิจรับเหมาก่อสร้าง เรามีทีมงาน ผู้เชี่ยวชาญด้านบัญชีที่พร้อมให้คำปรึกษาและดูแลบัญชีของคุณ อย่างครอบคลุม เพื่อให้คุณมันใจได้ว่าข้อมูลทางการเงินของคุณ มีความถูกต้อง แม่นยำ และเป็นไปตามมาตรฐานสากล",
+    // lang_7:"",
+    // lang_8:"",
+    // lang_9:"",
+    // lang_10:"",
+    // lang_11:"",
+    // lang_12:"",
+    // lang_13:"",
+    // lang_14:"",
+    // lang_15:"",
+    // lang_16:"",
+    // lang_17:"",
+    // lang_18:"",
+    // lang_19:"",
+  }
+  
+  var en ={
+    lang_language:"Language: EN",
+    lang_Home:"Home",
+    lang_About:"About Us",
+    lang_Services:"Our Services",
+    lang_Work:"Our Portfolio",
+    lang_News:"News ",
+    lang_Contant:"Contact Us",
+
+    lang_item1:"Accounting & Tax Solutions ",
+    lang_item2:"Auditing Services",
+    lang_item3:"Tax Planning and System Implementation Services",
+    lang_item4:"Registration Services",
+    lang_item5:"Food Storage Facility Permit Application Services",
+    lang_item6:"Company Registration Services",
+    lang_item7:"Import-Export License Application Services ",
+    footer_1:"NRP My Account Co., Ltd.",
+    footer_2:" 115/8 Pruksa Rangsit-Wongwaen Village, Moo 2, Lam Phak Kut Subdistrict, Thanyaburi District, Pathum Thani Province",
+    footer_2_1:" Tel: 062-896-4535 Email: info@nrpmyaccount.com",
+    footer_3:"Triple Earn Accounting Office",
+    footer_4:"Accounting Consultant",
+    footer_5:"Monthly Accounting Services",
+    footer_6:"Annual Accounting Services",
+    footer_7:"Financial Statement System",
+    footer_8:"Triple Earn Accounting Office",
+    footer_9:"Accounting Consultant",
+    footer_10:"Monthly Accounting Services",
+    footer_11:"Annual Accounting Services",
+    footer_12:"Financial Statement System",
+
+
+    lang_1:"NRP MY ACCOUNT CO., LTD. ",
+    lang_2:"NRP  ",
+    lang_3:"MY ACCOUNT CO., LTD.  ",
+    lang_4:"Get to Know Us",
+    lang_5:"NRP My Account Co., Ltd.",
+    lang_6:" NRP My Account Co., Ltd. is a professional accounting firm with over 10 years of experience in providing comprehensive accounting services to businesses of all sizes, from SMEs and startups to multinational corporations in a variety of industries, including manufacturing, trading, services, and even construction. We have a team of accounting experts ready to provide advice and comprehensive accounting care to ensure that your financial information is accurate, precise, and in compliance with international standards.",
+    lang_7:"Call Now!",
+    lang_8:"Why Choose Us",
+    lang_9:"Professional Team",
+    lang_10:"Fast and Accurate",
+    lang_11:"Modern Technology",
+    lang_12:"10+ Years of Experience",
+    lang_13:"Worth the Investment",
+    lang_14:"Our Services",
+    lang_15:"Accounting, Financial Statement Closing, and Tax Filing",
+    lang_16:"Comprehensive accounting services, financial statement closing, and tax filing. Fast, accurate, transparent, and all-in-one service.",
+    lang_17:" Auditing Services",
+    lang_18:"Comprehensive auditing services by a team of expert accountants. Financial statement audits, audit reports, financial statement summary reports.",
+    lang_19:" Tax System Planning Services",
+    lang_20:" Comprehensive accounting system planning to establish a correct and efficient accounting system foundation.",
+    lang_21:" Registration Services",
+    lang_22:" Comprehensive business registration services. Business license application, taxpayer identification number, certificate of incorporation changes, social security matters.",
+    lang_23:"Food Storage License Application Services",
+    lang_24:" Food storage license application services, FDA notification, comprehensive operations until the final step.",
+    lang_25:" Company Registration Services",
+    lang_26:"Business registration from the first step to completion.",
+    lang_27:" Import-Export License Application Services",
+    lang_28:" Triple Earn Accounting Office provides accounting and tax services, including monthly accounting, annual accounting, and financial statement closing.",
+    typewriter:"Free consultation available.",
+    lang_29:"Call Now!",
+    lang_30:" News/Activities",
+    lang_31:" Our Portfolio",
+    lang_32:" NRP My Account Free consultation available.",
+    lang_33:"",
+    lang_34:"",
+    lang_35:"",
+    lang_36:"",
+  }
+    function renderlang(){
+      if(!localStorage.lang){
+        localStorage.setItem("lang","en")
+      }else{
+        $("#lang_language").text(settext("lang_language"));
+        $("#lang_Home").text(settext("lang_Home"));
+        $("#lang_About").text(settext("lang_About"));
+        $("#lang_Services").text(settext("lang_Services"));
+        $("#lang_Work").text(settext("lang_Work"));
+        $("#lang_News").text(settext("lang_News"));
+        $("#lang_Contant").text(settext("lang_Contant"));
+        $("#footer_2_1").text(settext("footer_2_1"));
+        // footer
+        $("#footer_1").text(settext("footer_1"));
+        $("#footer_2").text(settext("footer_2"));
+        $("#footer_3").text(settext("footer_3"));
+        $("#footer_4").text(settext("footer_4"));
+        $("#footer_5").text(settext("footer_5"));
+        $("#footer_6").text(settext("footer_6"));
+        $("#footer_7").text(settext("footer_7"));
+        $("#footer_8").text(settext("footer_8"));
+        $("#footer_9").text(settext("footer_9"));
+        $("#footer_10").text(settext("footer_10"));
+        $("#footer_11").text(settext("footer_11"));
+        $("#footer_12").text(settext("footer_12"));
+
+        
+        // pade index
+        $("#lang_item1").text(settext("lang_item1"));
+        $("#lang_item2").text(settext("lang_item2"));
+        $("#lang_item3").text(settext("lang_item3"));
+        $("#lang_item4").text(settext("lang_item4"));
+        $("#lang_item5").text(settext("lang_item5"));
+        $("#lang_item6").text(settext("lang_item6"));
+        $("#lang_item7").text(settext("lang_item7"));
+        $("#lang_1").text(settext("lang_1"));
+        $("#lang_2").text(settext("lang_2"));
+        $("#lang_3").text(settext("lang_3"));
+        $("#lang_4").text(settext("lang_4"));
+        $("#lang_5").text(settext("lang_5"));
+        $("#lang_6").text(settext("lang_6"));
+        $("#lang_7").text(settext("lang_7"));
+        $("#lang_8").text(settext("lang_8"));
+        $("#lang_9").text(settext("lang_9"));
+        $("#lang_10").text(settext("lang_10"));
+        $("#lang_11").text(settext("lang_11"));
+        $("#lang_12").text(settext("lang_12"));
+        $("#lang_13").text(settext("lang_13"));
+        $("#lang_14").text(settext("lang_14"));
+        $("#lang_15").text(settext("lang_15"));
+        $("#lang_16").text(settext("lang_16"));
+        $("#lang_17").text(settext("lang_17"));
+        $("#lang_18").text(settext("lang_18"));
+        $("#lang_19").text(settext("lang_19"));
+        $("#lang_20").text(settext("lang_20"));
+        $("#lang_21").text(settext("lang_21"));
+        $("#lang_22").text(settext("lang_22"));
+        $("#lang_23").text(settext("lang_23"));
+        $("#lang_24").text(settext("lang_24"));
+        $("#lang_25").text(settext("lang_25"));
+        $("#lang_26").text(settext("lang_26"));
+        $("#lang_27").text(settext("lang_27"));
+        $("#lang_28").text(settext("lang_28"));
+        $("#lang_29").text(settext("lang_29"));
+        $("#typewriter").text(settext("typewriter"));
+        $("#lang_30").text(settext("lang_30"));
+        $("#lang_31").text(settext("lang_31"));
+        $("#lang_32").text(settext("lang_32"));
+      }
+    }
+  
+    function settext(key) {
+    if (localStorage.lang == "en") {
+      return en[key];
+    } else {
+      return th[key];
+    }
+  }
+  
+    function togglelang() {
+    if (localStorage.lang == "en") localStorage.setItem("lang", "th");
+    else if (localStorage.lang == "th") localStorage.setItem("lang", "en");
+  
+    renderlang();
+    return "now language: " + localStorage.lang;
+  }
+  
+  renderlang()
